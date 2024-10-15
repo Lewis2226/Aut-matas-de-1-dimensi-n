0a823d1b069e5c103b5947e8663567ad86914c89
Lewis2226 <131185164+Lewis2226@users.noreply.github.com>
2024-10-14 19:20:37 -0600

    base de los autómatas

---
+++ Assets/Scripts/WolframCelularAutomata.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolframCelularAutomata : MonoBehaviour
+{
+    public int width;
+    public int height;
+    [SerializeField] private int ruleNum;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
