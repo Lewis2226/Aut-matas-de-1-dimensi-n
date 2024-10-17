28ca279b1d9f79ee74515e131e3dac83c51f484a
Lewis2226 <131185164+Lewis2226@users.noreply.github.com>
2024-10-16 20:42:30 -0600

    prueba de la regla 30

--- Assets/Scripts/WolframCelularAutomata.cs
+++ Assets/Scripts/WolframCelularAutomata.cs
@@ -4,82 +4,140 @@ using UnityEngine;
 
 public class WolframCelularAutomata : MonoBehaviour
 {
-    public int width;
-    public int height;
+    public int width; 
+    public int height; 
     public Color aliveColor;
     public Color deadColor;
     public GameObject cellPrefab;
-    private bool[,] totalCells;
-    private GameObject[,] cellsOnScreen;
-    private bool runnigSimulation;
-    private int simulationNum =0;
-    [SerializeField] private int ruleNum;
+    private bool[,] totalCells; 
+    private bool[] currentGeneration; 
+    private GameObject[,] cellsOnScreen; 
+    private bool[] nextState; 
     [SerializeField] private float waitTime;
 
     // Start is called before the first frame update
     void Start()
     {
+        totalCells = new bool[height, width]; 
+        currentGeneration = new bool[width];
+        nextState = new bool[width]; 
+        cellsOnScreen = new GameObject[height, width];
+
         CreateBoard();
+        LifeGiver();  
+        StartSimulation(); 
     }
 
-    // Update is called once per frame
-    void Update()
+    void CreateBoard()
     {
-        
-    }
-
-    void CreateBoard()//Crea el tablero con el tamaño dado de ancho y largo
-    {
-        totalCells= new bool [width, height];
-        cellsOnScreen = new GameObject[width, height];
-        for (int i = 0; i < width; i++) 
+        for (int y = 0; y < height; y++)
         {
-            for (int j = 0; j < height; j++)
+            for (int x = 0; x < width; x++) 
             {
-                Vector3 position = new Vector3(i, j, 0);
-                GameObject cell = Instantiate(cellPrefab, position, Quaternion.identity);
-                cell.GetComponent<SpriteRenderer>().color = deadColor;
+                Vector3 position = new Vector3(x, -y, 0);
+                GameObject cell = Instantiate(cellPrefab, position, Quaternion.identity); 
+                cell.GetComponent<SpriteRenderer>().color = deadColor; 
+                cellsOnScreen[y, x] = cell; 
             }
         }
     }
 
-    void NumberToBinary(int numberRule)//Convierte el numero dado a binario
+    void LifeGiver()// Da vida de forma aleatoria a las células
     {
-
-    }
-
-    void SetLife()//Da vida a los autómatas base como  dice la regla
-    {
-
-    }
-
-    void CheckRule(int rule)//Revisa la regla selecionada
-    {
-
-    }
-
-    void ShowSimulation(int number)//Muestra la simulacion en pantalla.
-    {
-
-    }
-
-    public void StopSimulation()//Detiene la corutina
-    {
-        runnigSimulation = false;
-    }
-
-    public void ContinueSimulation()//Continua la corutina
-    {
-        runnigSimulation = true;
-    }
-
-    IEnumerator NewGeneration()//Genera la nueva simulacion despues deun lapso de tiempo
-    {
-        while (runnigSimulation) 
+        for (int i = 0; i < width; i++)
         {
-            CheckRule(ruleNum);
-            ShowSimulation(simulationNum);
-            yield return new WaitForSeconds(waitTime);
+         int rnd = Random.Range(0, 2);
+         if(rnd == 0)
+         {
+           currentGeneration[i] = true;
+         }
+         else
+         {
+           currentGeneration[i] = false;
+         }
+            
+        }
+
+        
+        for (int x = 0; x < width; x++)
+        {
+            totalCells[0, x] = currentGeneration[x]; 
         }
     }
+
+    bool CheckRule(bool left, bool center, bool right) // Revisa la regla dada en este caso la 30
+    {
+        if (left && center && right) return false;  // 111 -> 0
+        if (left && center && !right) return false; // 110 -> 0
+        if (left && !center && right) return false; // 101 -> 0
+        if (left && !center && !right) return true; // 100 -> 1
+        if (!left && center && right) return true;  // 011 -> 1
+        if (!left && center && !right) return true; // 010 -> 1
+        if (!left && !center && right) return true; // 001 -> 1
+        if (!left && !center && !right) return false;//000 -> 0
+        return false;
+    }
+
+    void NextGen()//Genera la siguinte generacion usando la regla
+    {
+
+        for (int i = 0; i < width; i++)
+        {
+            bool left = (i == 0) ? false : currentGeneration[i - 1];  
+            bool center = currentGeneration[i];                       
+            bool right = (i == width - 1) ? false : currentGeneration[i + 1];  
+
+            nextState[i] = CheckRule(left, center, right);
+        }
+
+        
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = height - 1; y > 0; y--)
+            {
+                totalCells[y, x] = totalCells[y - 1, x];
+            }
+            totalCells[0, x] = nextState[x];
+        }
+
+        for (int i = 0; i < width; i++)
+        {
+            currentGeneration[i] = nextState[i];
+        }
+    }
+
+    public void ShowSimulation()//Muestra la simulación
+    {
+       
+        for (int y = 0; y < height; y++) 
+        {
+            for (int x = 0; x < width; x++) 
+            {
+                if (totalCells[y, x]) 
+                {
+                    cellsOnScreen[y, x].GetComponent<SpriteRenderer>().color = aliveColor;
+                }
+                else 
+                {
+                    cellsOnScreen[y, x].GetComponent<SpriteRenderer>().color = deadColor;
+                }
+            }
+        }
+    }
+
+    public void StartSimulation()
+    {
+        StartCoroutine(NewGeneration());
+    }
+
+    IEnumerator NewGeneration()//Muestra la siguiente generacion
+    {
+        while (true)
+        {
+            NextGen();
+            ShowSimulation(); 
+            yield return new WaitForSeconds(waitTime); 
+        }
+    }
+
 }
